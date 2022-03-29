6e550997bd6139f2e283aa45773701207fe4bcb1
kuba98975 <kuba123wlazlo@gmail.com>
2022-03-29 16:11:14 +0200

    Add CSS, learn Templating vs. Layouts

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const app = express();
 //robienie templejtów za pomocą EJS, app możemy używać dopiero po stworzeniu const app
 
 //pusta zmienna dla renderowania nowych punktów listy
-var items = [];
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+//pusta lista dla podstrony work
+var workItems =[];
 
 app.set('view engine', 'ejs');
 
@@ -21,6 +23,8 @@ app.use(express.urlencoded());
 
 //metodą na wysyłanie całych plików jest res.sendFile, __dirname do uzyskiwania ścieżki dostępu w której obecnie pracujemy
 
+//definiujemy folder z plikami publicznymi by zaimplementować css
+app.use(express.static("public"));
 
 
 app.get("/", function(req, res) {
@@ -40,15 +44,31 @@ var day = today.toLocaleDateString("en-US", options);
 
 //o i tu renderujemy templejt do którego dokładamy zmienną day w zależności od tego, jaki mamy dzień tygodnia
 //przekazujemy też zmienną do renderowania kolejnych pozycji listy
-  res.render("list", {kindOfDay: day, newListItem: items});
+  res.render("list", {listTitle: day, newListItems: items});
 });
 
 //zbieramy kolejną pozycję za pomocą metody post odwołującej sie do formsa w htmlu
 app.post("/", function(req, res) {
   const item = req.body.next_item;
+  console.log(req.body)
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
   items.push(item);
   res.redirect("/");
+}
 });
+//sprawdzamy czy value które idzie od buttona czyli tytuł listy jest właśnie work czy coś innego i jeżeli work to dodajemy nowe pozycje listy do nowej listy workList
+
+
+//get dla podstrony work
+app.get("/work", function(req, res) {
+  const title = "Work List";
+  res.render("list", {listTitle: title, newListItems: workItems});
+})
+
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
